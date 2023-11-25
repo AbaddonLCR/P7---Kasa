b73016fb52d36bbf02b672d2a65215a3c997660e
Sellier Alexandre <a.sellier333@laposte.net>
2023-11-25 15:18:20 +0100

    Mise en place des assets, des components, des datas, des pages et des routes

---
+++ src/pages/lodging/Lodging.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Lodging = () => {
+     return (
+          <div>
+               <h1>Hébergements</h1>
+          </div>
+     );
+};
+
+export default Lodging;
