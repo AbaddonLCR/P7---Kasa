aa81daf1eed37de081315d8ff3bdbec637205128
Sellier Alexandre <a.sellier333@laposte.net>
2023-12-20 16:01:20 +0100

    Mise en place des components rating et tags pour le lodging

---
+++ src/components/rating/Rating.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './rating.scss';
+
+const Rating = () => {
+     return (
+          <div>
+               
+          </div>
+     );
+};
+
+export default Rating;
---
+++ src/components/tags/Tags.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './tags.scss';
+
+const Tags = ({tag}) => {
+     return (
+          <span>
+               {tag}
+          </span>
+     );
+};
+
+export default Tags;
